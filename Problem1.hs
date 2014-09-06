9c8f0f55822f6ca4aef84b3cc2c791bbe8f2a977
Alexis Chantreau <achantreau@hotmail.fr>
2014-09-06 13:03:16 +0100

    Update Problem1.hs - more general case

--- Problem1.hs
+++ Problem1.hs
@@ -1,6 +1,6 @@
 -- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 -- The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
-problem1 = sumMultiplesOf 3 + sumMultiplesOf 5 - sumMultiplesOf 15
+problem1 = sumMultiplesOf 3 999 + sumMultiplesOf 5 999 - sumMultiplesOf 15 999
 
-sumMultiplesOf n = n * (999 `div` n) * ((999 `div` n) + 1) `div` 2
+sumMultiplesOf n limit = n * (limit `div` n) * ((limit `div` n) + 1) `div` 2
